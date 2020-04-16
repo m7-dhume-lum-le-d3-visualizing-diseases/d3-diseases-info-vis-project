52ba1da1abdfd0dfdc91b9af92a9d64a2abced30
Kenny Le <ken.le@hotmail.com>
2020-04-16 02:05:06 -0400

    fixed alvin's non-working people ratio

--- scripts/WorldMap.js
+++ scripts/WorldMap.js
@@ -7,6 +7,9 @@ var selectedMaxYear = 2018;
 
 var selectedDisease = "malaria"; // default, gets updated on dropdown
 
+var tempSummedCases = 0;
+var tempSummedDeaths = 0;
+
 var min = 10000;
 var max = 10000000;
 
@@ -145,7 +148,7 @@ function displayMap(disease) {
                 // Try to SUM CASES
                 try{
                     var i;
-                    var tempSummedCases = 0;
+                    tempSummedCases = 0;
                     var difference = 0;
                     for (i = selectedMinYear; i < selectedMaxYear; i++) {
                         difference = 2018-i; // ie. Min = 2013, Max = 2015. This iteration does: 2018-2013 = 4, 2018-2014 = 3, 2018-2015 = 2, and sums data[4], data[3], data[2].
@@ -168,7 +171,7 @@ function displayMap(disease) {
                     */
                 }
                 catch(e){
-                    var tempSummedCases = 0; // Exception handle if it does not exist
+                    tempSummedCases = 0; // Exception handle if it does not exist
                 }                                                
                 try{
                     return color(tempSummedCases);  // Try to return cases to color the region accordingly based on sum of cases
@@ -199,7 +202,7 @@ function displayMap(disease) {
                 // Try to SUM CASES
                 try{
                     var i;
-                    var tempSummedCases = 0;
+                    tempSummedCases = 0;
                     var difference = 0;
                     for (i = selectedMinYear; i < selectedMaxYear; i++) {
                         difference = 2018-i;
@@ -217,7 +220,7 @@ function displayMap(disease) {
                 // Try to SUM DEATHS
                 try{
                     var i;
-                    var tempSummedDeaths = 0;
+                    tempSummedDeaths = 0;
                     var difference = 0;
                     for (i = selectedMinYear; i < selectedMaxYear; i++) {
                         difference = 2018-i;
@@ -230,8 +233,14 @@ function displayMap(disease) {
                     }
                 }
                 catch(e){
-                    var tempSummedDeaths = 0; // Exception handle if it does not exist
+                    tempSummedDeaths = 0; // Exception handle if it does not exist
                 }
+
+                // Save to session to load for PeopleRatio page later
+                console.log(tempSummedCases);
+                sessionStorage.setItem("tempSummedCasesKey",tempSummedCases);
+                sessionStorage.setItem("tempSummedDeathsKey",tempSummedDeaths);    
+                showPeople(); // Update people
 
                 // DISPLAY hover text box of year range, cases, and deaths
                 worldMap.classed("hidden", false)
@@ -258,65 +267,3 @@ function mergeData (jsona, jsonb) {
     });
     return jsona;
 }
-
-function showPeople() {
-    var person = document.getElementById("person")
-    var infectedIcon = '<i class="fa fa-male" style="font-size:60px;color:red;"></i>';
-    var icon = '<i class="fa fa-male" style="font-size:60px;"></i>';
-    var pictures = "";
-    var text = "";
-    var CongoCases = 127939;
-    var tempSummedDeaths = 0;
-    var i;
-    var tempSummedCases = 0;
-    var difference = 0;
-    var tempSummedDeaths = 0;
-    try {
-        for (i = selectedMinYear; i < selectedMaxYear; i++) {
-            difference = 2018-i; // ie. Min = 2013, Max = 2015. This iteration does: 2018-2013 = 4, 2018-2014 = 3, 2018-2015 = 2, and sums data[4], data[3], data[2].
-            try{
-                tempSummedCases += d.data[difference].cases; // We can sum for example, sum the years 2013-2015 with data[4] + data[3] + data[2].
-            }
-            catch(e){
-                tempSummedCases += 0;
-            }
-        }
-    }
-    catch(e){
-        tempSummedCases = 0; // Exception handle if it does not exist
-    }
-    i = 0;
-    difference = 0;
-    try{
-        for (i = selectedMinYear; i < selectedMaxYear; i++) {
-            difference = 2018-i;
-            try{
-                tempSummedDeaths += d.data[difference].deaths;
-            }
-            catch(e){
-                tempSummedDeaths += 0;
-            }
-        }
-    }
-    catch(e){
-        tempSummedDeaths = 0; // Exception handle if it does not exist
-    }
-    //var CongoPop = 81340000;
-    var DeathRate = tempSummedDeaths / tempSummedCases * 100;
-    //var CongoInfectedRate = tempSummedCases / CongoPop * 10;
-    //document.getElementById("test").innerHTML = CongoInfectedRate;
-    for (i = 0; i < 100; i++) {
-        if (i < Math.round(DeathRate)) {
-            pictures += infectedIcon;
-        }
-        else {
-            pictures += icon;
-        }
-    }
-    document.getElementById("person").innerHTML = pictures;
-    console.log(document.getElementById("test"));
-    console.log(DeathRate);
-    text = DeathRate + " out of 100 people in this country infected with this diseases died";
-    document.getElementById("test").innerHTML = text;
-}
-
