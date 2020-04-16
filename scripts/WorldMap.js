770becfd93f1d23f34c4eddeeac97f602effe4d8
alpinerum <35735448+alpinerum@users.noreply.github.com>
2020-04-15 23:08:30 -0400

    updated people visualization

--- scripts/WorldMap.js
+++ scripts/WorldMap.js
@@ -3,6 +3,11 @@
 // Also referencing https://blog.soshace.com/mapping-the-world-creating-beautiful-maps-and-populating-them-with-data-using-d3-js/, please modify to our own
 var selectedMinYear = 2000; // default, gets updated on slider
 var selectedMaxYear = 2018;
+<<<<<<< Updated upstream
+=======
+var selectedDisease = "malaria"; // default, gets updated on dropdown
+
+>>>>>>> Stashed changes
 var min = 10000;
 var max = 10000000;
 
@@ -243,13 +248,47 @@ function showPeople() {
     var pictures = "";
     var text = "";
     var CongoCases = 127939;
-    var CongoDeaths = 27458;
+    var tempSummedDeaths = 0;
+    var i;
+    var tempSummedCases = 0;
+    var difference = 0;
+    var tempSummedDeaths = 0;
+    try {
+        for (i = selectedMinYear; i < selectedMaxYear; i++) {
+            difference = 2018-i; // ie. Min = 2013, Max = 2015. This iteration does: 2018-2013 = 4, 2018-2014 = 3, 2018-2015 = 2, and sums data[4], data[3], data[2].
+            try{
+                tempSummedCases += d.data[difference].cases; // We can sum for example, sum the years 2013-2015 with data[4] + data[3] + data[2].
+            }
+            catch(e){
+                tempSummedCases += 0;
+            }
+        }
+    }
+    catch(e){
+        tempSummedCases = 0; // Exception handle if it does not exist
+    }
+    i = 0;
+    difference = 0;
+    try{
+        for (i = selectedMinYear; i < selectedMaxYear; i++) {
+            difference = 2018-i;
+            try{
+                tempSummedDeaths += d.data[difference].deaths;
+            }
+            catch(e){
+                tempSummedDeaths += 0;
+            }
+        }
+    }
+    catch(e){
+        tempSummedDeaths = 0; // Exception handle if it does not exist
+    }
     var CongoPop = 81340000;
-    var CongoDeathRate = CongoDeaths / CongoCases * 10;
-    var CongoInfectedRate = CongoCases / CongoPop * 10;
+    var DeathRate = tempSummedDeaths / tempSummedCases * 10;
+    //var CongoInfectedRate = tempSummedCases / CongoPop * 10;
     //document.getElementById("test").innerHTML = CongoInfectedRate;
     for (i = 0; i < 10; i++) {
-        if (i < Math.round(CongoDeathRate)) {
+        if (i < Math.round(DeathRate)) {
             pictures += infectedIcon;
         }
         else {
@@ -257,6 +296,8 @@ function showPeople() {
         }
     }
     document.getElementById("person").innerHTML = pictures;
-    text = Math.round(CongoDeathRate) + " out of 10 people in Congo infected with malaria died";
+    console.log(document.getElementById("test"));
+    text = DeathRate + " out of 10 people in Congo infected with malaria died";
     document.getElementById("test").innerHTML = text;
+    
 }
